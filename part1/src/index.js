d69f2f64519afa97420397c29f8c3438666d9e8e
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-08-05 22:59:11 +0200

    exercise 1.1: course information, step 1

---
+++ part1/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Content = (props) => (
+  <table>
+    <tbody>
+      {props.parts.map(p =>
+        <tr key={p.name}>
+          <td>{p.name}</td>
+          <td>{p.exercises}</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
+
+const Total = (props) => (
+  <p>Number of exercises: {props.parts.map(p => p.exercises).reduce((a, b) => a + b)}</p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts = [
+    { name: part1, exercises: exercises1 },
+    { name: part2, exercises: exercises2 },
+    { name: part3, exercises: exercises3 },
+  ]
+
+  return (
+    <>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
