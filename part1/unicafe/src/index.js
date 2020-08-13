4f0b5fd264991ae0c99cd8f452164ca434324cee
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-08-13 22:32:12 +0200

    exercise 1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,23 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({ good, bad, neutral }) => {
+  const allRatings = good + neutral + bad
+  const averageRating = allRatings === 0 ? 0 : (good - bad) / allRatings
+  const positive = allRatings === 0 ? 0 : good / allRatings
+
+  return (
+    <>
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}<br />
+      all {allRatings}<br />
+      average {averageRating}<br />
+      positive {positive * 100} %
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -9,10 +26,6 @@ const App = () => {
   const incrementState = (value, setter) => () => {
     setter(value + 1)
   }
-
-  const allRatings = good + neutral + bad
-  const averageRating = allRatings === 0 ? 0 : (good - bad) / allRatings
-  const positive = allRatings === 0 ? 0 : good / allRatings
 
   return (
     <div>
@@ -23,12 +36,7 @@ const App = () => {
         <button onClick={incrementState(bad, setBad)}>bad</button>
       </p>
       <h2>statistics</h2>
-      good {good}<br />
-      neutral {neutral}<br />
-      bad {bad}<br />
-      all {allRatings}<br />
-      average {averageRating}<br />
-      positive {positive * 100} %
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
