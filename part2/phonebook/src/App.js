bc3b674f6bb95e020007096003cad66cb58d3a05
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-09-12 00:10:48 +0200

    exercise 2.6 phonebook step 1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+    const inputRef = React.createRef()
+
+    const submit = (e) => {
+        e.preventDefault();
+        inputRef.current.focus();
+        setNewName('')
+        const trimmed = newName.trim()
+        if (trimmed !== '') {
+            setPersons([...persons, { name: trimmed }])
+        }
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={submit}>
+                <div>
+                    name: <input ref={inputRef} autofocus="true" value={newName} onChange={(e) => setNewName(e.target.value)} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map(p => <li key={p.name}>{p.name}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default App
