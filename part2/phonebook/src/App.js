db33ff9f0d982bbc8d10db4b75f71365d47f9853
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-12-15 09:58:42 +0100

    exercise 2.8 phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '1234567' }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
     const inputRef = React.createRef()
 
     const submit = (e) => {
@@ -17,9 +18,10 @@ const App = () => {
             return
         }
 
-        setNewName('')
         if (trimmed !== '') {
-            setPersons([...persons, { name: trimmed }])
+            setNewName('')
+            setNewNumber('')
+            setPersons([...persons, { name: trimmed, number: newNumber.trim() }])
         }
     }
 
@@ -28,16 +30,32 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={submit}>
                 <div>
-                    name: <input ref={inputRef} autofocus="true" value={newName} onChange={(e) => setNewName(e.target.value)} />
+                    name: <input ref={inputRef} autoFocus={true} value={newName} onChange={(e) => setNewName(e.target.value)} />
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            <ul>
-                {persons.map(p => <li key={p.name}>{p.name}</li>)}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Number</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {persons.map(p =>
+                        <tr key={p.name}>
+                            <td>{p.name}</td>
+                            <td>{p.number}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 }
