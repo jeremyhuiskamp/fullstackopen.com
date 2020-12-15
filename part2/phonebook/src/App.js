964d205e2c7d368a3573f64ce18ca1e119c4311f
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-12-15 10:57:03 +0100

    exercise 2.10 phonebook step 5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,59 @@
 import React, { useState } from 'react'
 
+const Filter = ({ filter, setFilter }) =>
+    <div>
+        filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+    </div>
+
+const PersonForm = ({ newName, setNewName, newNumber, setNewNumber, tryNewName }) => {
+    const inputRef = React.createRef()
+
+    const submit = (e) => {
+        e.preventDefault()
+        inputRef.current.focus()
+
+        tryNewName()
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <div>
+                name: <input ref={inputRef} autoFocus={true} value={newName} onChange={(e) => setNewName(e.target.value)} />
+            </div>
+            <div>
+                number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+const Persons = ({ persons, filter }) => {
+    const filteredPersons = persons.filter(p =>
+        p.name.toLowerCase().includes(filter.trim().toLowerCase())
+    )
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Number</td>
+                </tr>
+            </thead>
+            <tbody>
+                {filteredPersons.map(p =>
+                    <tr key={p.name}>
+                        <td>{p.name}</td>
+                        <td>{p.number}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas', number: '1234567' },
@@ -10,12 +64,8 @@ const App = () => {
     const [filter, setFilter] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
-    const inputRef = React.createRef()
 
-    const submit = (e) => {
-        e.preventDefault();
-        inputRef.current.focus();
-
+    const tryNewName = () => {
         const trimmed = newName.trim()
         if (persons.some(p => p.name === trimmed)) {
             alert(`${trimmed} is already added to phonebook`)
@@ -32,42 +82,17 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <div>
-                filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
-            </div>
-            <h2>add a new</h2>
-            <form onSubmit={submit}>
-                <div>
-                    name: <input ref={inputRef} autoFocus={true} value={newName} onChange={(e) => setNewName(e.target.value)} />
-                </div>
-                <div>
-                    number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Number</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {persons
-                        .filter(p =>
-                            p.name.toLowerCase().includes(filter.trim().toLowerCase())
-                        )
-                        .map(p =>
-                            <tr key={p.name}>
-                                <td>{p.name}</td>
-                                <td>{p.number}</td>
-                            </tr>
-                        )}
-                </tbody>
-            </table>
+
+            <Filter filter={filter} setFilter={setFilter} />
+
+            <h3>add a new</h3>
+            <PersonForm
+                newName={newName} setNewName={setNewName}
+                newNumber={newNumber} setNewNumber={setNewNumber}
+                tryNewName={tryNewName} />
+
+            <h3>Numbers</h3>
+            <Persons persons={persons} filter={filter} />
         </div>
     )
 }
