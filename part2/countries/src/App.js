f90519db0262eaef6122da40c546b5d6f296b44e
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-12-15 17:19:30 +0100

    exercise 2.14 countries step 3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,46 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
+const Weather = ({ country }) => {
+  const capitalOverrides = {
+    "USA": "Washington DC"
+  }
+
+  const city = capitalOverrides[country.alpha3Code] ?? country.capital;
+
+  const [weather, setWeather] = useState(null);
+  useEffect(() => {
+    if (city) {
+      axios.get('http://api.weatherstack.com/current', {
+        params: {
+          access_key: process.env.REACT_APP_WEATHERSTACK_API_KEY,
+          query: city,
+        }
+      }).then(rsp => {
+        setWeather(rsp.data)
+      }).catch(console.log);
+    }
+  }, [city]);
+
+  if (weather === null || !(weather.current)) {
+    // TODO: generally better handling of error responses
+    return <></>;
+  }
+
+  // Using weather.request.query to show what weatherstack thinks we
+  // requested.  It's not always the same thing as the restcountries.eu capital.
+  return <div>
+    <h2>Weather in {weather.request.query}</h2>
+    <p><b>temperature:</b> {weather.current.temperature} Celsius</p>
+    <p>
+      <img
+        src={weather.current.weather_icons[0]}
+        alt={weather.current.weather_descriptions[0]} />
+    </p>
+    <p><b>wind:</b> {weather.current.wind_speed} kph direction {weather.current.wind_dir}</p>
+  </div>;
+}
+
 const Country = ({ country }) => {
   return <div>
     <h2>{country.name}</h2>
@@ -11,6 +51,7 @@ const Country = ({ country }) => {
       {country.languages.map(l => <li key={l.iso639_2}>{l.name}</li>)}
     </ul>
     <img width="100px" alt={"flag of " + country.name} src={country.flag} />
+    <Weather country={country} />
   </div>
 };
 
