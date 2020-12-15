21da0171d41f2f686690c6665a5361c6a0e78efb
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2020-12-15 10:14:25 +0100

    exercise 2.9 phonebook step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '1234567' }
+        { name: 'Arto Hellas', number: '1234567' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' },
     ])
+    const [filter, setFilter] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const inputRef = React.createRef()
@@ -28,6 +32,10 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={submit}>
                 <div>
                     name: <input ref={inputRef} autoFocus={true} value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -48,12 +56,16 @@ const App = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {persons.map(p =>
-                        <tr key={p.name}>
-                            <td>{p.name}</td>
-                            <td>{p.number}</td>
-                        </tr>
-                    )}
+                    {persons
+                        .filter(p =>
+                            p.name.toLowerCase().includes(filter.trim().toLowerCase())
+                        )
+                        .map(p =>
+                            <tr key={p.name}>
+                                <td>{p.name}</td>
+                                <td>{p.number}</td>
+                            </tr>
+                        )}
                 </tbody>
             </table>
         </div>
