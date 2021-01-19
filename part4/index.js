eddf7acb5f073dee5a4feb2b7c38590353b9020b
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-01-19 23:45:31 +0100

    exercise 4.2: blog list, step 2: factor out app.js

--- part4/index.js
+++ part4/index.js
@@ -1,25 +1,9 @@
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const http = require('http');
+const app = require('./app');
 
-const blogsRouter = require('./controllers/blogs');
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
-
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`);
+const server = http.createServer(app);
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 });
