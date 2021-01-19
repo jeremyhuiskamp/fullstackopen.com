b4cea57b78877c30bdca0e99b876bc73214842e8
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-01-20 00:00:16 +0100

    exercise 4.2: blog list, step 2: add middleware

--- part4/app.js
+++ part4/app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -21,11 +22,11 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
-// app.use(middleware.requestLogger);
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 
-// app.use(middleware.unknownEndpoint);
-// app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
