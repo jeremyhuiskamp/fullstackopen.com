641677a9f279eb4611392058fc3db3b6cc8b3a56
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-01-20 21:58:21 +0100

    exercise 4.5: helper functions and unit tests, step3

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -65,3 +65,96 @@ describe('total likes', () => {
         expect(result).toEqual(6);
     });
 });
+
+describe('favourite blog', () => {
+    test('null for no blogs', () => {
+        expect(listHelper.favouriteBlog([])).toEqual(null);
+    });
+
+    test('only blog is favourite', () => {
+        expect(listHelper.favouriteBlog([
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 1,
+            },
+        ])).toEqual({
+            title: 'title1',
+            author: 'author1',
+            likes: 1,
+        });
+    });
+
+    test('2 likes is better than 1', () => {
+        expect(listHelper.favouriteBlog([
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 1,
+            },
+            {
+                id: 'id2',
+                title: 'title2',
+                author: 'author2',
+                url: 'url2',
+                likes: 2,
+            },
+        ])).toEqual({
+            title: 'title2',
+            author: 'author2',
+            likes: 2,
+        });
+    });
+
+    test('2 likes is better than 1, even if it comes first', () => {
+        expect(listHelper.favouriteBlog([
+            {
+                id: 'id2',
+                title: 'title2',
+                author: 'author2',
+                url: 'url2',
+                likes: 2,
+            },
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 1,
+            },
+        ])).toEqual({
+            title: 'title2',
+            author: 'author2',
+            likes: 2,
+        });
+    });
+
+    test('return last amongst ties', () => {
+        // this behaviour isn't mandated, but lets make sure we understand
+        // what we've implemented.
+        expect(listHelper.favouriteBlog([
+            {
+                id: 'id2',
+                title: 'title2',
+                author: 'author2',
+                url: 'url2',
+                likes: 2,
+            },
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 2,
+            },
+        ])).toEqual({
+            title: 'title1',
+            author: 'author1',
+            likes: 2,
+        });
+    });
+});
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,4 +1,11 @@
 const totalLikes = blogs =>
     blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
-module.exports = { totalLikes };
+const favouriteBlog = blogs =>
+    blogs.reduce((fave, blog) => fave?.likes > blog.likes ? fave : {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes,
+    }, null);
+
+module.exports = { totalLikes, favouriteBlog };
