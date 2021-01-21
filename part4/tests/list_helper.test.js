2403ea0b37a45985fd5081ed8f6d8e18dfe4f598
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-01-21 18:17:51 +0100

    exercise 4.6: helper functions and unit tests, step4

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,4 +1,5 @@
 const listHelper = require('../utils/list_helper');
+const _ = require('lodash');
 
 describe('total likes', () => {
     test('when list has only one blog, equals the likes of that', () => {
@@ -67,8 +68,8 @@ describe('total likes', () => {
 });
 
 describe('favourite blog', () => {
-    test('null for no blogs', () => {
-        expect(listHelper.favouriteBlog([])).toEqual(null);
+    test('undefined for no blogs', () => {
+        expect(listHelper.favouriteBlog([])).toEqual(undefined);
     });
 
     test('only blog is favourite', () => {
@@ -155,6 +156,132 @@ describe('favourite blog', () => {
             title: 'title1',
             author: 'author1',
             likes: 2,
+        });
+    });
+});
+
+describe('most blogs', () => {
+    test('no blogs means undefined', () => {
+        expect(listHelper.mostBlogs([])).not.toBeDefined();
+    });
+
+    test('one blog is an easy competition', () => {
+        expect(listHelper.mostBlogs([
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 2,
+            },
+        ])).toEqual({
+            author: 'author1',
+            blogs: 1,
+        });
+    });
+
+    test('one author is still an easy competition', () => {
+        expect(listHelper.mostBlogs([
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 2,
+            },
+            {
+                id: 'id2',
+                title: 'title2',
+                author: 'author1',
+                url: 'url2',
+                likes: 42,
+            },
+        ])).toEqual({
+            author: 'author1',
+            blogs: 2,
+        });
+    });
+
+    test('lots of data', () => {
+        // TODO: log a randomization seed for reproducible shuffling
+        expect(listHelper.mostBlogs(_.shuffle([
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 2,
+            },
+            {
+                id: 'id2.1',
+                title: 'title2.1',
+                author: 'author2',
+                url: 'url2.1',
+                likes: 42,
+            },
+            {
+                id: 'id2.2',
+                title: 'title2.2',
+                author: 'author2',
+                url: 'url2.2',
+                likes: 9,
+            },
+            {
+                id: 'id3.1',
+                title: 'title3.1',
+                author: 'author3',
+                url: 'url3.1',
+                likes: 3,
+            },
+            {
+                id: 'id3.2',
+                title: 'title3.2',
+                author: 'author3',
+                url: 'url3.2',
+                likes: 93,
+            },
+            {
+                id: 'id3.3',
+                title: 'title3.3',
+                author: 'author3',
+                url: 'url3.3',
+                likes: 39,
+            },
+        ]))).toEqual({
+            author: 'author3',
+            blogs: 3,
+        });
+    });
+
+    test('in case of tie, first alphabetical author wins', () => {
+        const blogs = [
+            {
+                id: 'id1',
+                title: 'title1',
+                author: 'authorA',
+                url: 'url1',
+                likes: 1,
+            },
+            {
+                id: 'id2',
+                title: 'title2',
+                author: 'authorB',
+                url: 'url2',
+                likes: 2,
+            },
+        ];
+
+        expect(listHelper.mostBlogs(blogs)).toEqual({
+            author: 'authorA',
+            blogs: 1,
+        });
+
+        blogs.reverse();
+
+        // same output:
+        expect(listHelper.mostBlogs(blogs)).toEqual({
+            author: 'authorA',
+            blogs: 1,
         });
     });
 });
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const totalLikes = blogs =>
     blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
@@ -6,6 +8,36 @@ const favouriteBlog = blogs =>
         title: blog.title,
         author: blog.author,
         likes: blog.likes,
-    }, null);
+    }, undefined);
 
-module.exports = { totalLikes, favouriteBlog };
+// chooseBest returns the best thing from things, using chooseFunc to choose
+// between any two things.  If there is only 1 thing, it is the best.  If
+// there are zero things, the best is undefined.
+const chooseBest = (things, chooseFunc) =>
+    things.reduce((a, b) => a === undefined ? b : chooseFunc(a, b), undefined);
+
+const mostBlogs = blogs => {
+    // { "author1": 1, "author2": 2 }
+    const counted = _.countBy(blogs, b => b.author);
+
+    // [ { author: "author1", blogs: 1 }, { author: "author2", blogs: 2 } ]
+    const authors = _.toPairs(counted).map(([author, blogs]) => ({ author, blogs }));
+
+    return chooseBest(authors, (a1, a2) => {
+        if (a1.blogs > a2.blogs) {
+            return a1;
+        } else if (a2.blogs > a1.blogs) {
+            return a2;
+        } else if (a1.author < a2.author) {
+            return a1;
+        } else {
+            return a2;
+        }
+    });
+};
+
+module.exports = {
+    totalLikes,
+    favouriteBlog,
+    mostBlogs,
+};
