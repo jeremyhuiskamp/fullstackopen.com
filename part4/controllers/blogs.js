475f04ad5acd07636f08c8e8aa72e9be133cad3b
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-01-31 21:44:18 +0100

    exercise 4.20: blog list expansions, step8
    This goes a bit beyond the exercise description to also verify the token
    and look up the requesting user from the db.  Seems pragmatic, since we
    obviously want to do the same validation in multiple places...

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,38 +1,27 @@
 const router = require('express').Router();
-const jwt = require('jsonwebtoken');
 require('express-async-errors');
 const Blog = require('../models/blog');
-const User = require('../models/user');
+const { requireAuthenticatedUser } = require('../utils/middleware');
 
 router.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { blogs: 0 });
     response.json(blogs);
 });
 
-router.post('/', async (request, response) => {
-    const authenticatedUser = await getAuthenticatedUser(request);
-    if (!authenticatedUser) {
-        return response.status(401).json({ error: 'token missing or invalid' });
-    }
-
+router.post('/', requireAuthenticatedUser, async (request, response) => {
     const blog = new Blog({
         ...request.body,
-        user: authenticatedUser._id,
+        user: request.user._id,
     });
     const result = await blog.save();
 
-    authenticatedUser.blogs.push(result._id);
-    await authenticatedUser.save();
+    request.user.blogs.push(result._id);
+    await request.user.save();
 
     response.status(201).json(result);
 });
 
-router.delete('/:id', async (request, response) => {
-    const authenticatedUser = await getAuthenticatedUser(request);
-    if (!authenticatedUser) {
-        return response.status(401).json({ error: 'token missing or invalid' });
-    }
-
+router.delete('/:id', requireAuthenticatedUser, async (request, response) => {
     const blog = await Blog.findByIdAndDelete(request.params.id);
     if (blog) {
         response.json(blog);
@@ -41,12 +30,7 @@ router.delete('/:id', async (request, response) => {
     }
 });
 
-router.patch('/:id', async (request, response) => {
-    const authenticatedUser = await getAuthenticatedUser(request);
-    if (!authenticatedUser) {
-        return response.status(401).json({ error: 'token missing or invalid' });
-    }
-
+router.patch('/:id', requireAuthenticatedUser, async (request, response) => {
     const blog = await Blog.findById(request.params.id);
     if (!blog) {
         return response.status(404).end();
@@ -56,24 +40,5 @@ router.patch('/:id', async (request, response) => {
     const savedBlog = await blog.save();
     response.json(savedBlog);
 });
-
-const getAuthenticatedUser = async request => {
-    const authorization = request.get('authorization');
-    if (!authorization?.toLowerCase().startsWith('bearer ')) {
-        return undefined;
-    }
-
-    const token = authorization.substring(7);
-    try {
-        const decodedToken = jwt.verify(token, process.env.SECRET);
-
-        return await User.findById(decodedToken.id);
-    } catch (e) {
-        if (e.name === 'JsonWebTokenError') {
-            return undefined;
-        }
-        throw e;
-    }
-};
 
 module.exports = router;
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const requestLogger = (request, response, next) => {
     logger.info(request.method, request.path, request.body);
@@ -10,7 +12,6 @@ const unknownEndpoint = (request, response) => {
 };
 
 const errorHandler = (error, request, response, next) => {
-
     if (error.name === 'CastError') {
         logger.info(error.message);
         return response.status(400).send({ error: 'malformatted id' });
@@ -24,8 +25,60 @@ const errorHandler = (error, request, response, next) => {
     next(error);
 };
 
+// Checks authorization header for a bearer token, rejecting responses with an
+// invalid token, and storing validated tokens in request.token.
+//
+// Requests with no bearer token are allowed through, but request.token is not
+// set.  This should be to fine to put in front of all api calls.
+const validateToken = (request, response, next) => {
+    const authorization = request.get('authorization');
+    if (!authorization?.toLowerCase().startsWith('bearer ')) {
+        return next();
+    }
+
+    const token = authorization.substring(7);
+    try {
+        request.token = jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+        if (e.name === 'JsonWebTokenError') {
+            return response
+                .status(401)
+                .json({ error: 'token missing or invalid' });
+        }
+        throw e;
+    }
+    next();
+};
+
+// Checks for request.token (populated by validateToken above) and looks up the
+// user that is referenced, storing it in request.user.
+//
+// Requests with no token or tokens referencing an unknown user are rejected.
+//
+// Use this in front of apis that can only be called by authenticated users.
+const requireAuthenticatedUser = async (request, response, next) => {
+    if (!request.token?.id) {
+        return response
+            .status(401)
+            .json({ error: 'token missing or invalid' });
+    }
+
+    const user = await User.findById(request.token.id);
+    if (!user) {
+        return response
+            .status(401)
+            .json({ error: 'token missing or invalid' });
+    }
+
+    request.user = user;
+
+    next();
+};
+
 module.exports = {
     requestLogger,
     unknownEndpoint,
-    errorHandler
+    errorHandler,
+    validateToken,
+    requireAuthenticatedUser,
 };
