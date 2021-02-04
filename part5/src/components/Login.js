7f26cdd5fc077dd5ef2dec8f8b49eb31a4172a0f
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-02-04 23:15:15 +0100

    exercise 5.2: blog list frontend, step 2
    Logout was already implemented, so this just adds local storage.

--- part5/src/components/Login.js
+++ part5/src/components/Login.js
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import loginService from '../services/login';
 
 const Login = ({ user, setUser }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errMsg, setErrMsg] = useState(null);
+
+    // Check local storage for existing user and register listener for future
+    // changes:
+    useEffect(() => {
+        const checkForLoggedInUser = () => {
+            const localStorageUser = localStorage.getItem('loggedInUser');
+            if (localStorageUser) {
+                console.log('found loggedInUser in local storage');
+                setUser(JSON.parse(localStorageUser));
+            } else {
+                console.log('found no loggedInUser in local storage');
+                setUser(null);
+            }
+        };
+        checkForLoggedInUser();
+        window.addEventListener('storage', () => {
+            console.log('checking local storage for changes to loggedInUser');
+            checkForLoggedInUser();
+        });
+    }, []);
+
+    // Whenever the user changes, update local storage.
+    // NB: this will fire after we setUser if we detect a change from another
+    // tab, and we'll write the same value back to local storage.  Writing the
+    // same value back does not appear to trigger the event listener in other
+    // tabs, but if it did, we'd probably trigger an infinite loop...
+    useEffect(() => {
+        if (user) {
+            localStorage.setItem('loggedInUser', JSON.stringify(user));
+        } else {
+            localStorage.removeItem('loggedInUser');
+        }
+    }, [user]);
 
     const tryLogin = async event => {
         event.preventDefault();
@@ -22,7 +55,6 @@ const Login = ({ user, setUser }) => {
         }
     };
 
-    console.log('user', user);
     if (user) {
         return <div>
             Welcome {user.username}.
