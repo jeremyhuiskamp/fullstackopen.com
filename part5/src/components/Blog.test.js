29ac8439cb88bffbd984741ff4835e69206155d2
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-02-15 22:47:13 +0100

    exercise 5.14: blog list tests, step2

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
 test('renders only title and author by default', () => {
@@ -22,4 +22,30 @@ test('renders only title and author by default', () => {
     expect(component.container).toHaveTextContent('author1');
     expect(component.container).not.toHaveTextContent('user1');
     expect(component.container).not.toHaveTextContent('2 👍');
+});
+
+test('renders likes and user when expanded', () => {
+    const blog = {
+        title: 'title1',
+        author: 'author1',
+        url: 'url1',
+        likes: 2,
+        user: {
+            username: 'user1',
+        },
+    };
+
+    const handler = jest.fn();
+
+    const component = render(
+        <Blog blog={blog} like={handler} remove={handler} />
+    );
+
+    fireEvent.click(component.container.firstChild);
+    expect(handler.mock.calls).toHaveLength(0);
+
+    expect(component.container).toHaveTextContent('title1');
+    expect(component.container).toHaveTextContent('author1');
+    expect(component.container).toHaveTextContent('user1');
+    expect(component.container).toHaveTextContent('2 👍');
 });
